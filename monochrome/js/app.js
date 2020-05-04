af108d6912c3947ffdaae80f98587a9f63b7e443
sandracatalina <60328895+sandracatalina@users.noreply.github.com>
2020-05-05 02:26:02 +0300

    mobile first

--- monochrome/js/app.js
+++
@@ -1,6 +0,0 @@
-var burger = document.querySelector('.user-controls .fa-bars');
-var body = document.body;
-
-burger.addEventListener('click', function() {
-  body.classList.toggle('menu-open');
-});
